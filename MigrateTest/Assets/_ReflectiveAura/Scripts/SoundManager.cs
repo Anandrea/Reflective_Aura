42f00f32ec3a51e2ecd38d4408c6ee72c19418d4
enyafunke <50293129+enyafunke@users.noreply.github.com>
2024-02-28 18:05:56 +0100

    audio lerp

--- MigrateTest/Assets/_ReflectiveAura/Scripts/SoundManager.cs
+++ MigrateTest/Assets/_ReflectiveAura/Scripts/SoundManager.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] AudioSource backgroundSource;
-    [SerializeField] AudioSource effectSource;
+    [SerializeField] AudioSource backgroundSourceAngry;
+    [SerializeField] AudioSource backgroundSourceHappy;
+    [SerializeField] AudioSource backgroundSourceSad;
+    [SerializeField] AudioSource effectSourceAngry;
+    [SerializeField] AudioSource effectSourceHappy;
+    [SerializeField] AudioSource effectSourceSad;
     [SerializeField] AudioClip happy;
     [SerializeField] AudioClip sad;
     [SerializeField] AudioClip angry;
@@ -15,31 +19,54 @@ public class SoundManager : MonoBehaviour
 
     public void AngrySound()
     {
-        backgroundSource.clip = angry;
-        backgroundSource.Play();
-        effectSource.clip = fire;
-        effectSource.Play();
+        backgroundSourceAngry.clip = angry;
+        backgroundSourceAngry.Play();
+        StartCoroutine(StartFade(backgroundSourceAngry, 5, 1));
+        effectSourceAngry.clip = fire;
+        effectSourceAngry.Play();
+        StartCoroutine(StartFade(effectSourceAngry, 5, 1));
     }
 
     public void HappySound()
     {
-        backgroundSource.clip = happy;
-        backgroundSource.Play();
-        effectSource.clip = birds;
-        effectSource.Play();
+        backgroundSourceHappy.clip = happy;
+        backgroundSourceHappy.Play();
+        StartCoroutine(StartFade(backgroundSourceHappy, 5, 1));
+        effectSourceHappy.clip = birds;
+        effectSourceHappy.Play();
+        StartCoroutine(StartFade(effectSourceHappy, 5, 1));
     }
 
     public void SadSound()
     {
-        backgroundSource.clip = sad;
-        backgroundSource.Play();
-        effectSource.clip = rain;
-        effectSource.Play();
+        backgroundSourceSad.clip = sad;
+        backgroundSourceSad.Play();
+        StartCoroutine(StartFade(backgroundSourceSad, 5, 1));
+        effectSourceSad.clip = rain;
+        effectSourceSad.Play();
+        StartCoroutine(StartFade(effectSourceSad, 5, 1));
     }
 
     public void NeutralSound()
     {
-        backgroundSource.Stop();
-        effectSource.Stop();
+        StartCoroutine(StartFade(backgroundSourceAngry, 5, 0));
+        StartCoroutine(StartFade(backgroundSourceHappy, 5, 0));
+        StartCoroutine(StartFade(backgroundSourceSad, 5, 0));
+        StartCoroutine(StartFade(effectSourceAngry, 5, 0));
+        StartCoroutine(StartFade(effectSourceHappy, 5, 0));
+        StartCoroutine(StartFade(effectSourceSad, 5, 0));
+    }
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 }
